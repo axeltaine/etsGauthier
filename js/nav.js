7425d43549fbbee13ed1af79b90c0c350f2ac8b8
axel <48438507+axeltaine@users.noreply.github.com>
2019-12-09 21:06:00 +0100

    first com

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+
+        $(document).ready(function() {
+        // Transition effect for navbar 
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 50) { 
+              $('.navbar').addClass('transparent');
+          } else {
+              $('.navbar').removeClass('transparent');
+          }
+        });
+});
+ 
