7227d947f0fc5d77b85660f83a5750ddddf3ec46
axel <48438507+axeltaine@users.noreply.github.com>
2019-12-10 10:29:38 +0100

    news pages

--- js/slider.js
+++ js/slider.js
@@ -2,30 +2,4 @@ $('#myCarousel').carousel({
     interval: 3000,
  });
 
- $(document).on("click", '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-  });
-
-  $(document).ready(function(){
-    $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4
-        }
-      }, {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 3
-        }
-      }]
-    });
-  });
+ 
