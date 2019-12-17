8428ac2c32c6c97f646a6d6c126a9cea9b67cc51
axel <48438507+axeltaine@users.noreply.github.com>
2019-12-17 14:35:15 +0100

    modif

--- js/nav.js
+++ js/nav.js
@@ -11,3 +11,14 @@
         });
 });
  
+$(document).ready(function() {
+  // Transition effect for navbar 
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if($(this).scrollTop() > 50) { 
+        $('.dropdown-menu').addClass('transparent');
+    } else {
+        $('.dropdown-menu').removeClass('transparent');
+    }
+  });
+});
